a877502a6184e48cca63d6242fc7099e0fb87643
Chandan koranga <chandan.koranga.2025@outlook.com>
2026-06-03 01:35:21 +0530

    fix homepage import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Homepage from "./pages/Homepage";
+import Homepage from './pages/Homepage';
 
 export default function App() {
   return (
